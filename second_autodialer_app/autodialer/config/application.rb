b99fa291296882d648ab62bcecf1dcd53887fdd1
ihamdaan <azizhamdaan@gmail.com>
2025-11-06 23:24:52 +0530

    Fix: load full Rails stack for ActiveJob

--- second_autodialer_app/autodialer/config/application.rb
+++ second_autodialer_app/autodialer/config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
-require "rails"
-require "action_controller/railtie"
-require "action_view/railtie"
+
+require "rails/all"
 
 require_relative "../lib/in_memory_store"
 
@@ -9,12 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Autodialer
   class Application < Rails::Application
-    config.load_defaults 7.1
+    config.load_defaults 8.1
     config.api_only = false
-    
-    # Disable ActiveRecord
-    config.generators.orm false
-    
+
     # Load lib directory
     config.autoload_paths << Rails.root.join('lib')
     config.eager_load_paths << Rails.root.join('lib')
